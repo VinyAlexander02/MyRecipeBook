0b5586c0773f80b96b1af5651b0d290d35aa0070
VinyAlexander02 <vinicius.malex2002@gmail.com>
2026-02-02 21:21:30 -0300

    creating mapper and crip password

---
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace MyRecipeBook.Application.Services.Cryptography;
+
+public class PasswordEncripter
+{
+    public string Encrypt(string password)
+    {
+        var addKey = "ABC";
+        var newPassword = $"{password}{addKey}";
+        var bytes = Encoding.UTF8.GetBytes(newPassword);
+        var hashBytes = SHA512.HashData(bytes);
+
+        return StringBytes(hashBytes);
+    }
+
+    private static string StringBytes(byte[] bytes)
+    {
+        var sb = new StringBuilder();
+        foreach (var b in bytes)
+        {
+            var hex = b.ToString("x2");
+            sb.Append(hex);
+        }
+        return sb.ToString();
+    }
+}
+
